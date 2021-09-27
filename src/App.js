2db7abecc0b863fbe92ec6f981e9ae2b67c495f9
leocomeron <leocomeron@gmail.com>
2021-09-26 23:49:09 -0300

    Cambio de idioma

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-// import FotoPerfil from '../src/FotoPerfil.jpg'
-// import IconReact from '../src/IconReact.png'
-// import IconMongo from '../src/IconMongo.png'
-// import classes from '../src/App.module.css'
-
-import Spanish from '../src/components/Spanish.js'
-import English from '../src/components/English.js'
-
+import Spanish from './components/Principal.js'
 import { Route } from 'react-router-dom'
 
-// VER SI TRAIGO LA VARIABLE LENGUAJE TRUE O FALSE Y PONER UN IF ACA
 
 function App() {
 
@@ -16,9 +8,6 @@ function App() {
         <div>
             <Route path='/' exact>
                 <Spanish />
-            </Route>
-            <Route path='/en'>
-                <English />
             </Route>
         </div >
 
